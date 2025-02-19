59401374b96d1ed8b56b031368544732fadabb35
Guillermo1804 <memosanchez101@gmail.com>
2025-02-19 14:39:03 -0600

    Pagina de Inicio Completa
    En este push se terminó lo que es la implementación basica de la pagina de inicio, implementando el sidebar, el menu superior y los iconos para el cierre de sesión y/o para viajar a otras vistas

--- src/app/screens/home-component/hom-component.component.ts
+++ src/app/screens/home-component/hom-component.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-hom-component',
@@ -18,5 +18,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
   styleUrl: './hom-component.component.scss'
 })
 export class HomComponentComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
